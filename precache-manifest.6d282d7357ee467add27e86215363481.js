a7d642fec702e4b4c77febe4230912de1d3abb5e
Suji Roselet <suji.roselet@iinerds.com>
2020-04-26 17:51:39 +0530

    Updates

---
+++ precache-manifest.6d282d7357ee467add27e86215363481.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ece3a223c6bc32956102af9ceb27290",
+    "url": "/restaurantTriangle/index.html"
+  },
+  {
+    "revision": "b0889e597f358bbbe5b7",
+    "url": "/restaurantTriangle/static/css/main.3dd000f5.chunk.css"
+  },
+  {
+    "revision": "9b57efa0e8f8d8518fff",
+    "url": "/restaurantTriangle/static/js/2.58769c0f.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/restaurantTriangle/static/js/2.58769c0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0889e597f358bbbe5b7",
+    "url": "/restaurantTriangle/static/js/main.734d3ccd.chunk.js"
+  },
+  {
+    "revision": "80a12dc4267db0ba6604",
+    "url": "/restaurantTriangle/static/js/runtime-main.8b61f64e.js"
+  }
+]);
